f8d80e3992223d0258385d5f1efedcf6113602ea
ezana73 <ezana49@gmail.com>
2022-09-08 07:26:54 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,6 +9,6 @@
  */
 int main(void)
 {        
-	puts("\"Programming is like like building a multiligual puzzle,\n");
+	puts("\"Programming is like like building a multiligual puzzle");
 	return (0);
 }
