57059bd3d168c554f18947b501b8872f75a3ec19
ezana73 <ezana49@gmail.com>
2022-09-08 10:54:51 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,11 +9,11 @@
 */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
+char a;
+int b;
+long int c;
+long long int d;
+float e;
 
 printf("Size of a char: %ld byte(s)\n", sizeof(a));
 printf("Size of an int: %ld byte(s)\n", sizeof(b));
