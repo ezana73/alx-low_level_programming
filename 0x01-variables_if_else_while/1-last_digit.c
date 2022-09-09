165897ef81970e01d306c8a7d2d636c1c68a1e41
ezana73 <ezana49@gmail.com>
2022-09-09 06:37:25 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
-/*
-*main - Entry point
+/**
+*main - print if the number is positive,zero and negative
 *
 *Return: 0
 */
@@ -17,13 +16,16 @@ n = rand() - RAND_MAX / 2;
 l = n % 10;
 
 if (l > 5)
-printf("Last digit of %i is %i and greater than 5\n", n , l);
-
+{
+printf("Last digit of %d is %d and greater than 5\n", n, l);
+}
 else if (l == 0)
-printf("Last digit of %i is %i and is 0\n", n , l);
-
-else
-printf("Last digit of %i is %i and is less than 6 and 0\n" n , l);
-
+{
+printf("Last digit of %d is %d and is 0\n", n, l);
+}
+else 
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+}
 return (0);
 }
