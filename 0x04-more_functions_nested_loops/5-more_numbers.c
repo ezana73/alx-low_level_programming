bafb200cd8e4f34288f8edb766780787c0168337
ezana73 <ezana49@gmail.com>
2022-09-15 09:35:18 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,14 +13,14 @@ void more_numbers(void)
 	
 	for (n1 = 0; n1 < 10; n1++)
 	{
-		for (n2 = 0; n2 <= 14; n2++)
-		{
-			if (n2 > 9)
-			{
-				putchar((n2 / 10) + '0');
-			}
-			putchar((n2 % 10) + '0');
-		}
-		putchar(10);
+	for (n2 = 0; n2 <= 14; n2++)
+	{
+	if (n2 > 9)
+	{
+	putchar((n2 / 10) + '0');
+	}
+	putchar((n2 % 10) + '0');
+	}
+	putchar(10);
 	}
 }
