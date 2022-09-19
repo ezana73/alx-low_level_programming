a4e296c5df5c6f9fdf22e1c0a7533ae5695b0cdc
ezana73 <ezana49@gmail.com>
2022-09-19 09:35:59 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_print - reverse string
+ * @s: parameter
+ */
+
+void rev_string(char *s)
+{
+	char tmp;
+	int i, length1. length2;
+	length1 = 0;
+	length2 = 0;
+
+	while (s[length1] != '\0')
+		length1++;
+	length2 = length1 - 1;
+		for (i = 0; i < length1; i++)
+		{
+			tmp = s[i];
+			s[i] = s[length2];
+			s[length2] = tmp;
+			length2 -= 1;
+		}
+}
