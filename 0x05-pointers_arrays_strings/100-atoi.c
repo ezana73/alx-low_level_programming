19c802f1c5b59997ce7cfe997dc3dd38316780b1
ezana73 <ezana49@gmail.com>
2022-09-19 12:30:53 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -28,7 +28,7 @@ int _atoi(char *s)
 	     	{
 			num = num * 10 + (*temp - '0');
 				temp++;
-		} while (*temp >= '0' && *temp <= '9)';
+		} while (*temp >= '0' && *temp <= '9');
 				}
 				return (num * sign);
 				}
