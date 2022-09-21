91163bff1f04394d9ed8f737bb182c9a9264eb54
ezana73 <ezana49@gmail.com>
2022-09-21 09:58:51 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,7 +4,7 @@
 /**
  *_strcat -concatenates the string pointed to by @src to
  *to end of the string pointed to by the @dest
- *@dest: - the string that will be appended
+ *@dest: the string that will be appended
  *@src: the string to be concatenates 
  *
  *Return: returns poineer to @dest
