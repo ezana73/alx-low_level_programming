4cc9beada51a68ff488181abcfc5715c4670a0f4
ezana73 <ezana49@gmail.com>
2022-09-21 06:55:54 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * leet - function that encode string
+ * @str: a string that will be encoded
+ * Retirn: encode string
+ */
+
+char *leet(char *)
+{
+	int index1 = 0, index2;
+	char leet[8] = {'o','L', '?', 'E', 'A', '?', '?', 'T'};
+
+	while (str[++index])
+	{
+		for (index2 = 0; index2 <= 7; index2++)
+		{
+			if (str[index1] == leet[index2] ||
+			str[index1] - 32 == leet[index2])
+				str[index1] = index2 + '0';
+		}
+	}
+	return (str);
+}
