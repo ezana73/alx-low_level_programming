5fd678fb4b3877bdea40ce3123fd1fbee758c060
ezana73 <ezana49@gmail.com>
2022-09-22 11:16:55 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -17,9 +17,9 @@ char *cap_string(char *str)
 
 		if (str[index - 1] == ' ' ||
 				str[index - 1] == '\t' ||
-		 		str[index - 1] == '\n' ||
+				str[index - 1] == '\n' ||
 				str[index - 1] == ',' ||
-		  		str[index - 1] == ';' ||
+				str[index - 1] == ';' ||
 				str[index - 1] == '.' ||
 				str[index - 1] == '!' ||
 				str[index - 1] == '?' ||
