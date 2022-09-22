550a1e04216d89016009064b29b3e7c1e1b8af4c
ezana73 <ezana49@gmail.com>
2022-09-22 10:33:53 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -27,26 +27,26 @@ void print_buffer(char *b, int size)
 		if ((index % 2) != 0 && index != 0)
 			printf(" ");
 		}
-
-	for (index = 0; index < 10; index++)
-	{
-		if ((index + byte) >= size)
+		
+		for (index = 0; index < 10; index++)
+		{
+			if ((index + byte) >= size)
 			break;
-
-		else if (*(b + index + byte) >= 31 &&
-			 *(b + index + byte) <= 126)
-			printf("%c", *(b + index + byte));
-
-		else
-			printf(".");
+			
+			else if (*(b + index + byte) >= 31 &&
+				 *(b + index + byte) <= 126)
+				printf("%c", *(b + index + byte));
+			
+			else
+				printf(".");
+		}
+		
+		if (byte >= size)
+			continue;
+		
+		printf("\n");
 	}
-
-	if (byte >= size)
-		continue;
-
-	printf("\n");
-	}
-
+	
 	if (size <= 0)
 		printf("\n");
 }
