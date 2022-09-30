263914878bfbb6ad098343ba868291f2986c8476
ezana73 <ezana49@gmail.com>
2022-09-30 05:26:11 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - adds positive numbers[C
+*@argc:parameter
+*@argv: an array pointed to string
+*Return: zero
+*/
+
+int main(int argc, char *argv[])
+{
+	int sum = 0, num, i, j, k;
+
+	for (i = 0; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
+				printf("%s\n", Error);
+				return (1);
+			}
+		}
+	}
+	for (k = 1; k < argc; k++)
+	{
+		num = atoi(argv[k]);
+		sum += num
+	}
+	printf("%d\n", sum);
+	return (0);
+}
