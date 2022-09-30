4daaee409ba02250682e017a26245839e94d1acf
ezana73 <ezana49@gmail.com>
2022-09-30 04:39:15 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main - prints a number of arguments passed in
+*@argc: parameter
+*@argv: pointer to string
+*Return: zero
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
