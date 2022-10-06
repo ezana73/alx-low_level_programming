5ee70af6bbf16fc05647885db5641cd852471845
ezana73 <ezana49@gmail.com>
2022-10-06 04:52:41 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -5,7 +5,7 @@
 *_realloc - reallocate memory block by using malloc and free
 *@ptr: pointer to memory preveously allocated with a
 *call to malloc :malloc(old_size)
-*@old_size - size allocater for ptr
+*@old_size: size allocater for ptr
 *@new_size: size of byte new memory block
 *
 *Return: pointer to new memory, NULL or ptr
