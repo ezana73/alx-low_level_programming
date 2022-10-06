8bf476f045b94dbeeeb0d3bdbbb21b4c842b7de0
ezana73 <ezana49@gmail.com>
2022-10-06 03:47:26 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -49,7 +49,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		ptr[i + j] = s2[j];
 	ptr[i + j] = '\0';
 
-	return (ptr)
+	return (ptr);
 }
 
 
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,5 +8,6 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
 void _puts(char *str);
+int _strlen(char *string);
 
 #endif 
