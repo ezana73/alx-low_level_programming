2da3537c8aecaf4daca9fb892daddbbaee5ca1b3
ezana73 <ezana49@gmail.com>
2022-12-07 07:05:26 -0800

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,9 +8,26 @@
 
 void print_triangle(int size)
 {
-int hash, index;
+	int i = 1, ii;
 
-if (size > 0)
-{
-for (hash = 1; hash <= size; hash++)
-{
+	while (i <= size && size > 0)
+	{
+		ii = 0;
+		while (ii < size - i)
+		{
+			_putchar(' ');
+			ii++;
+		}
+		ii = 0;
+		while (ii < i)
+		{
+			_putchar('#');
+			ii++;
+		}
+
+		_putchar('\n');
+		i++;
+	}
+	if (i == 1)
+		_putchar('\n');
+}
